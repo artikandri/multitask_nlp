9817ccb6f97d3a4ebe8c025f53ee9ed6e2e0f16d
artikandri <argikartika@gmail.com>
2023-04-25 00:42:04 +0200

    feat: delete script

--- multitask_nlp/utils/zip_files.py
+++ multitask_nlp/utils/zip_files.py
@@ -39,7 +39,8 @@ def delete_unrelated_folders(checkpoint_names):
         for folderName, subfolders, filenames in os.walk(CHECKPOINTS_DIR):
             path = CHECKPOINTS_DIR / folderName
             if checkpoint_name not in subfolders and checkpoint_name not in folderName:
-                shutil.rmtree(path) 
+                print(f"---{folderName} on {path} will be deleted ---")               
+                # shutil.rmtree(folderName) 
 
 if __name__ == "__main__":
     df = get_df()
@@ -51,7 +52,15 @@ if __name__ == "__main__":
         paths.append(CHECKPOINTS_DIR / row["last_checkpoint_name"])
         checkpoint_names.append(row["last_checkpoint_name"])
     
+    checkpoint_names = ['floral-cosmos-11' 'smooth-serenity-10' 'sweet-wave-2' 'dancing-tiger-1'
+        'celestial-darkness-12' 'floating-fish-2' 'wise-bush-2'
+        'gallant-forest-10' 'bright-peony-2' 'charmed-dust-5' 'zesty-planet-5'
+        'stoic-puddle-6' 'distinctive-totem-3' 'genial-firefly-7'
+        'vibrant-aardvark-4' 'dry-grass-12' 'balmy-dream-3' 'light-bee-25'
+        'fanciful-donkey-7' 'bright-peony-2' 'distinctive-totem-3'
+        'genial-firefly-7' 'brisk-resonance-1' 'frosty-universe-25'
+        'dandy-totem-5' 'robust-salad-1' 'icy-wildflower-25' 'frosty-eon-1']
         
     delete_unrelated_folders(checkpoint_names)
-    zip_file(paths)
+    # zip_file(paths)
     
