97c8b7ed8224f11246da934d094541d26dd19549
artikandri <argikartika@gmail.com>
2023-05-22 17:27:01 +0200

    feat: mtl exps

--- multitask_nlp/experiments/single_task_exp/indonlu/nerp_ner_prosa2.py
+++ multitask_nlp/experiments/single_task_exp/indonlu/nerp_ner_prosa2.py
@@ -150,13 +150,50 @@ def run_experiments():
                     hparams_copy["learning_kind"] = 'STL'
                     hparams_copy["dataset"] = data_module.task_name
                     
-                    run_training(
-                        model, mtl_datamodule, hparams_copy, epochs, lr_rate, weight_decay,
-                        custom_callbacks=mtl_custom_callbacks,
-                        lightning_model_kwargs=used_lightning_model_kwargs,
-                        trainer_kwargs=trainer_kwargs,
-                        wandb_project_name=wandb_project_name
-                    )
+                    if analyze_latest_model:
+                        if  os.path.exists(ckpt_path):
+                            ckpt_files = os.listdir(ckpt_path)
+                            if ckpt_files:
+                                ckpt_file = ckpt_files[0]
+                                model2 = load_model(model, ckpt_path=ckpt_path/ckpt_file)
+                                size = get_size(model2)
+                                total_params, trainable_params = get_params(model2)
+                                exp_custom_callbacks = copy(custom_callbacks)
+                                
+                                
+                                predictions, avg_time = load_and_predict(
+                                    datamodule=data_module,
+                                    model=model2,
+                                    epochs=epochs,
+                                    lr=lr_rate,
+                                    logger=None,
+                                    exp_name=wandb_project_name,
+                                    weight_decay=weight_decay,
+                                    use_cuda=use_cuda,
+                                    custom_callbacks=exp_custom_callbacks,
+                                    lightning_model_kwargs=lightning_model_kwargs
+                                )
+                                
+                                results = [wandb_project_name,
+                                        f"ckpt_path: {ckpt_path}" ,
+                                        f"model size: {size}" ,
+                                        f"number of params: {total_params}",
+                                        f"number of trainable params: {trainable_params}" ,
+                                        f"average inference time: {avg_time}",
+                                        f"nr of epochs: {epochs}",
+                                        f"nr of rep: {i}"]
+                                        
+                                write_as_txt_file(results, f"{wandb_project_name}-{i}")  
+                        else:
+                            print("checkpoint path doesnt exist")
+                    else:
+                        run_training(
+                            model, mtl_datamodule, hparams_copy, epochs, lr_rate, weight_decay,
+                            custom_callbacks=mtl_custom_callbacks,
+                            lightning_model_kwargs=used_lightning_model_kwargs,
+                            trainer_kwargs=trainer_kwargs,
+                            wandb_project_name=wandb_project_name
+                        )
 
             # Multi-Task learning
             for multitask_dataset_type in multitask_dataset_types:
